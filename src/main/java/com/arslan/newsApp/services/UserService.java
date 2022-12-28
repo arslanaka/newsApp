a5c34c90f6690a128ba4008129cffefc3618fb21
Muhammad Arslan <geekaolma@gmail.com>
2022-12-28 21:30:18 +0500

    added user entity

---
+++ src/main/java/com/arslan/newsApp/services/UserService.java
@@ -0,0 +1,13 @@
+package com.arslan.newsApp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+
+
+
+
+
+}
---
+++ src/main/java/com/arslan/newsApp/services/implementation/UserServiceImplementation.java
@@ -0,0 +1,15 @@
+package com.arslan.newsApp.services.implementation;
+
+
+import com.arslan.newsApp.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImplementation implements UserService {
+
+
+
+
+
+
+}
